f20b241be3fd3e1ab60ed51632e156335496b76b
KhantTiKyiSectionD <N01709727@humber.ca>
2025-11-02 21:08:35 -0500

    creating connectdb.js

---
+++ boho-bistro-react/server/middlewares/connect-db.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+export const connectDb = async () => {
+  try {
+    const uri = process.env.MONGO_URI;
+    if (!uri) {
+      console.error("❌ MONGO_URI missing in .env");
+      process.exit(1);
+    }
+
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("✅ MongoDB Connected Successfully");
+    
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error.message);
+    process.exit(1);
+  }
+};
